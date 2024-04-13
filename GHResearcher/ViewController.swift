75345b037076462921fc1cc007ff43b8d23a0052
leo10k <leozinhomacedo41@gmail.com>
2024-04-13 04:49:42 -0300

    feat: set up project

---
+++ GHResearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHResearcher
+//
+//  Created by Leonardo Macedo on 13/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
