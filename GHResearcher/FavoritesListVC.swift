2d294ba93c500b9cf0534efd2c0636f8e11b2689
leo10k <leozinhomacedo41@gmail.com>
2024-04-15 08:51:08 -0300

    feat: create the TabBar and NavBar

---
+++ GHResearcher/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GHResearcher
+//
+//  Created by Leonardo Macedo on 15/04/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+
+}
---
+++ GHResearcher/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GHResearcher
+//
+//  Created by Leonardo Macedo on 15/04/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
