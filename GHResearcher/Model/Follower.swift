6e37e2641fa6ce6697652e525f4ee343f96e9084
leo10k <leozinhomacedo41@gmail.com>
2024-04-29 19:11:10 -0300

    feat: network call planning

---
+++ GHResearcher/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHResearcher
+//
+//  Created by Leonardo Macedo on 29/04/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
