4a388b3ca39a5be63ff478c2f80c56652b50cf94
leo10k <leozinhomacedo41@gmail.com>
2024-08-08 01:32:30 -0300

    feat: Create Network Call - Get Followers

--- GHResearcher/Screens/FollowerListVC.swift
+++ GHResearcher/Screens/FollowerListVC.swift
@@ -16,6 +16,14 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
