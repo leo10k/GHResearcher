9fd7dfd3830b1f0e0b889f8d5d1bd5319b59858d
leo10k <leozinhomacedo41@gmail.com>
2024-08-15 18:07:32 -0300

    feat: Add Loading View

--- GHResearcher/Screens/FollowerListVC.swift
+++ GHResearcher/Screens/FollowerListVC.swift
@@ -46,11 +46,12 @@ class FollowerListVC: UIViewController {
     }
     
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             
             switch result {
-                
             case .success(let followers):
                 if followers.count < 100 { hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
