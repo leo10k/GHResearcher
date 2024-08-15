aa7b7d09052263c293f63e9a95ad6ae492bb578c
leo10k <leozinhomacedo41@gmail.com>
2024-08-15 17:00:49 -0300

    feat: Download & Cache Avatar Images

--- GHResearcher/Managers/NetworkManager.swift
+++ GHResearcher/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Leonardo Macedo on 08/08/24.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
